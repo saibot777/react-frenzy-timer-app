18c230fb57b53783f0a78ddb856f07399beeb814
Stefan <stefantrajkovic777@gmail.com>
2016-11-01 23:44:22 +0100

    navigation implemented

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Frenzy Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/saibot777" target="_blank">Stefan Trajkovic</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Navigation;
